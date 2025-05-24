6e4f0db398e63535f28c0464c7d31259b45dde76
uvwbermudo <uvw.bermudo>
2025-05-24 23:19:18 +0800

    (chore) remove user in wsl domain

--- config/domains.lua
+++ config/domains.lua
@@ -20,8 +20,6 @@ return {
       {
          name = 'WSL:Ubuntu',
          distribution = 'Ubuntu',
-         username = 'kevin',
-         default_cwd = '/home/kevin',
          default_prog = { 'fish', '-l' },
       },
    },
